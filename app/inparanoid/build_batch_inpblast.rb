d5015d56b1882471365bb6e13d293b193791d4de
Shuji Shigenobu <sshigenobu@gmail.com>
2013-10-14 06:19:09 +0900

    bugfix inparanoid_blast.pl.patch and build_batch_inpblast.rb. The previous ver conflict formatdb if muliple inparanoid_blast.pl scripts were executed. In this ver formatdb was executed explicitly beforehand. The procedure of formatdb in inparanoid_blast.pl was commnetouted.

--- app/inparanoid/build_batch_inpblast.rb
+++ app/inparanoid/build_batch_inpblast.rb
@@ -1,9 +1,25 @@
 sp1 = ARGV[0]
 sp2 = ARGV[1]
 
+scripts = []
 [sp1, sp2, "#{sp1} #{sp2}", "#{sp2} #{sp1}"].each do |x|
   scr = "run_inparablast-#{x.gsub(/\s+/, '-')}.sh"
+  scripts << scr
   File.open(scr, "w"){|o|
     o.puts "perl inparanoid_blast.pl #{x} "
   }
 end
+
+scr0 = "run_formatdb.sh"
+File.open(scr0, "w"){|o|
+  o.puts "formatdb -i #{sp1}"
+  o.puts "formatdn -i #{sp2}"
+}
+
+sge = "sge_submission.sh"
+File.open(sge, "w"){|o|
+  o.puts "qsub -v PATH -N formatdb_#{sp1}-#{sp2} #{scr0}"
+  scripts.each do |s|
+    o.puts "qsub -v PATH -hold_jid formatdb_#{sp1}-#{sp2} #{s}"
+  end
+}
