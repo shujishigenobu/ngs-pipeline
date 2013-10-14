d846c8fda934b3b3ee953ef1878e5b3d655a8a2c
Shuji Shigenobu <sshigenobu@gmail.com>
2013-10-14 10:29:05 +0900

    bugfix: build_batch_inpblast.rb

--- app/inparanoid/build_batch_inpblast.rb
+++ app/inparanoid/build_batch_inpblast.rb
@@ -1,25 +1,23 @@
 sp1 = ARGV[0]
 sp2 = ARGV[1]
+template_dir = ARGV[2]
 
 scripts = []
 [sp1, sp2, "#{sp1} #{sp2}", "#{sp2} #{sp1}"].each do |x|
+  newdir = "#{template_dir}-#{x.gsub(/\s+/, '-')}"
+  cmd = "cp -a #{template_dir} #{newdir}"
+  system cmd
+
   scr = "run_inparablast-#{x.gsub(/\s+/, '-')}.sh"
   scripts << scr
   File.open(scr, "w"){|o|
-    o.puts "perl inparanoid_blast.pl #{x} "
+    o.puts "cd #{newdir}\nperl inparanoid_blast.pl #{x} "
   }
 end
 
-scr0 = "run_formatdb.sh"
-File.open(scr0, "w"){|o|
-  o.puts "formatdb -i #{sp1}"
-  o.puts "formatdb -i #{sp2}"
-}
-
 sge = "sge_submission.sh"
 File.open(sge, "w"){|o|
-  o.puts "qsub -v PATH -N formatdb_#{sp1}-#{sp2} #{scr0}"
   scripts.each do |s|
-    o.puts "qsub -v PATH -hold_jid formatdb_#{sp1}-#{sp2} #{s}"
+    o.puts "qsub -v PATH  #{s}"
   end
 }
