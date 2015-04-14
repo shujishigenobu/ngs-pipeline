c64b0bc9fe2c244336a9cfa2013caf2e678bd0db
Shuji Shigenobu <sshigenobu@gmail.com>
2015-04-14 20:40:56 +0900

    new tasks/bam_indexing

---
+++ tasks/bam_indexing/index_bam.rb
@@ -0,0 +1,31 @@
+#!/bin/ruby
+
+#=== config
+MAX_MEMORY = 30000000000
+#===
+
+
+$bam = ARGV[0]
+$name = ARGV[1]
+
+bam_unsorted = $bam
+
+
+STDERR.puts "# sorting bam"
+if $name
+  bam_sorted_prefix = $name + "." + File.basename(bam_unsorted, '.bam') + ".sorted"
+else
+  bam_sorted_prefix = File.basename(bam_unsorted, '.bam') + ".sorted"
+end
+
+cmd = "samtools sort -m #{MAX_MEMORY} #{bam_unsorted} #{bam_sorted_prefix}"
+IO.popen(cmd){}
+
+STDERR.puts "# indexing bam"
+p bam_sorted = bam_sorted_prefix + ".bam"
+cmd = "samtools index #{bam_sorted}"
+IO.popen(cmd)
+
+#File.delete(bam_unsorted)
+
+STDERR.puts "# completed"
