4089da92f8a7ef8d504506ae7f8e4681eee6eca0
Shuji Shigenobu <sshigenobu@gmail.com>
2020-09-16 17:47:51 +0900

    output name can be specified in commandline option

--- app/salmon/tmm_normalize.rb
+++ app/salmon/tmm_normalize.rb
@@ -1,4 +1,5 @@
 inputf = ARGV[0]
+outputf = (ARGV[1] || File.basename(inputf, ".txt") + ".norm.tmm.txt")
 
 TEMPR = "tmm_norm.template.R"
 
@@ -13,6 +14,7 @@ p cols
 scr = File.open(TEMPR).read
 scr.gsub!(/%INPUT_FILE%/, inputf)
 scr.sub!(/%COLS%/, "c(" + cols.map{|c| "\"#{c}\""}.join(", ") + ")")
+scr.sub!(/%OUTPUT_FILE%/, outputf)
 
 
 p rscript = TEMPR.sub(/\.template/, '')
